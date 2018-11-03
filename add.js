de345c5ccbb9de5487331320bfd34b4a3d8be2cd
hzy <497997526@qq.com>
2018-11-03 18:41:30 +0800

    asdasf

---
+++ add.js
@@ -0,0 +1,18 @@
+add  fun1
+function  aaa(){
+//sdfasdfasdfadsf
+console.log("aaa");
+}
+add  fun2
+function  bbb(){
+//sdfasdfasdfadsf
+console.log("bbb");
+}
+add  fun3
+function  ccc(){
+//sdfasdfasdfadsf
+}
+add  fun4
+function  ddd(){
+ 
+}
